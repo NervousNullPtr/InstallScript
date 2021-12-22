6fa987a549afed83a542718fef722c166f938ef9
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-22 22:37:09 +0100

    Finalise

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!bin/bash
 sudo -i;
-pacman -S git;
+# Installing Chaotic-AUR
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com;
+pacman-key --lsign-key FBA220DFC880C036;
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst';
+printf "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf;
+# End Chaotic-AUR
+pacman -Syu;
+pacman -S git fakeroot;
 git clone https://aur.archlinux.org/yay.git;
 cd yay;
 makepkg -si;
 cd ..;
 rm -rf yay;
 exit;
-yay -S nautilus discord code brave-bin fish kitty code-marketplace btop flameshot keepasxc cmatrix neofetch;
+yay -Syu;
+yay -S nautilus discord code brave-bin fish ttf-fira-code xorg-xrandr vim nano exfat-utils kitty polybar code-marketplace picom-git rustup texmaker texlive-most signal-desktop feh dmenu dunst cronie alsa-utils easyeffects btop flameshot curl keepasxc cmatrix neofetch;
 
 code --install-extension dan-c-underwood.arm;
 code --install-extension icrawl.discord-vscode;
@@ -28,3 +36,12 @@ mkdir ~Coding/Rust/LibRapidRust;
 
 git clone https://github.com/LibRapid/LibRapidRust ~/Coding/Rust/LibRapidRust;
 git clone https://github.com/NervousNullPtr/Types-Header ~/Coding/C++/Types-Header;
+
+curl https://wallpaperaccess.com/download/5k-hd-415851 -o ~/Bilder/Wallpaper.jpg;
+curl https://raw.githubusercontent.com/NervousNullPtr/InstallScript/master/kitty.conf -o ~/.config/kitty/kitty.conf;
+curl https://raw.githubusercontent.com/NervousNullPtr/InstallScript/master/i3-config -o ~/.config/i3/config;
+curl https://raw.githubusercontent.com/NervousNullPtr/InstallScript/master/config.rasi -o ~/.config/rofi/config.rasi;
+curl https://raw.githubusercontent.com/NervousNullPtr/InstallScript/master/picom.conf -o ~/.config/picom/picom.conf;
+curl https://raw.githubusercontent.com/NervousNullPtr/InstallScript/master/config.fish -o ~/.config/fish/config.fish;
+curl https://raw.githubusercontent.com/NervousNullPtr/InstallScript/master/polybar-config -o ~/.config/polyabr/config;
+curl https://raw.githubusercontent.com/NervousNullPtr/InstallScript/master/layout.sh -o ~/layout.sh;
