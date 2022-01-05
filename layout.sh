4bfb284907ea624e837214e4d0f1089fb29d0dff
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2022-01-05 17:18:36 +0100

    Changed layout.sh

--- layout.sh
+++ layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 -r 165 --primary --mode 1920x1080 --pos 1920x621 --rotate normal --output DP-2 -r 165 --mode 1920x1080 --pos 3840x0 --rotate right --output HDMI-1 -r 60 --off --output HDMI-2 --mode 1920x1080 --pos 0x621 --rotate normal --output DVI-D-1 --off
+xrandr --output DP-1 -r 165 --primary --mode 1920x1080 --pos 1920x621 --rotate normal --output DP-2 -r 165 --mode 1920x1080 --pos 3840x162 --rotate right --output HDMI-1 -r 60 --off --output HDMI-2 --mode 1920x1080 --pos 0x595 --rotate normal --output DVI-D-1 --off
